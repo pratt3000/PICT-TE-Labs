22ae42b41560d0c2449c2b08cc71f0020693cce1
pratt3000 <pratt3000@gmail.com>
2020-08-20 16:11:42 +0530

    minor

--- SDL/InventoryManagement/mysql/create_table.sql
+++ SDL/InventoryManagement/mysql/create_table.sql
@@ -8,6 +8,6 @@ insert into users values("admin", "admin123");
 
 select *from users;
 
-#--dummy entries
+
 #-- source /home/pratt3000/Documents/College Codes/PICT_TE-Labs/SDL/InventoryManagement/mysql/create_table.sql
 
