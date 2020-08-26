a42f4a219f7148aebf300f3cccf4a2fb35fad5aa
pratt3000 <pratt3000@gmail.com>
2020-08-26 12:27:51 +0530

    submitted successfully

--- DBMSL/a_4_JAVA/src/App.java
+++ DBMSL/a_4_JAVA/src/App.java
@@ -8,19 +8,22 @@ public class App {
         int repeat=1, choice;
 
         System.out.println("1.Create table");
-        System.out.println("2.Create view");
-        System.out.println("3.Create index");
+        System.out.println("2.Create Simple view");
+        System.out.println("3.Create Simple index");
         System.out.println("4.Create sequence");
         System.out.println("5.Create synonym");
         System.out.println("6.exit");
-        
+        System.out.println("7.Create Complex view");
+        System.out.println("8.Create Compound index");
+        System.out.println("9.Create Unique index");
+
         ex.reset();
         while(repeat==1){
             System.out.print("Enter option : ");
             choice = Integer.parseInt(in.nextLine());
             switch (choice) { 
                 case 1: 
-                    ex.create_table("sample");
+                    ex.create_table();
                     break; 
                 case 2: 
                     ex.create_view();
@@ -33,7 +36,16 @@ public class App {
                     break; 
                 case 5: 
                     ex.create_synonym(); 
-                    break; 
+                    break;
+                case 7:
+                    ex.create_complex_view(); 
+                    break;
+                case 8:
+                    ex.create_compound_index();
+                    break;
+                case 9:
+                    ex.create_unique_index();
+                    break;
                 default: 
                     break; 
                 }
--- DBMSL/a_4_JAVA/src/exe.java
+++ DBMSL/a_4_JAVA/src/exe.java
@@ -2,13 +2,19 @@ import java.sql.*;
 
 public class exe {
     
-    void create_table(String name){
+    void create_table(){
         try{
-            String stmnt = "create table "+name+" (id int not null,value varchar(20))";
             Class.forName("com.mysql.cj.jdbc.Driver");
             Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/A1_professor_schema","root","Hello@123");
             Statement stmt = con.createStatement();
-            stmt.executeUpdate(stmnt);
+            stmt.executeUpdate("drop table if exists sample");
+            stmt.executeUpdate("create table sample (id int not null,value varchar(20))");
+            
+            ResultSet r = stmt.executeQuery("select * from sample");
+            while(r.next()) {
+                System.out.println(r.getInt(1) + "\t" + r.getString(2));
+            }
+
             con.close();
         }catch(Exception e){System.out.println(e);}
     }
@@ -20,17 +26,25 @@ public class exe {
             Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/A1_professor_schema","root","Hello@123");
             Statement stmt = con.createStatement();
             stmt.executeUpdate(stmnt);
+            ResultSet r = stmt.executeQuery("select * from comp");
+            while(r.next()) {
+                System.out.println(r.getString(1) + "\t" + r.getString(2));
+            }
             con.close();
         }catch(Exception e){System.out.println(e);}
     }
 
     void create_index(){
         try{
-            String stmnt = "CREATE INDEX ind_1 ON professor(salary)";
+            String stmnt = "CREATE INDEX ind_1 ON professor(fname)";
             Class.forName("com.mysql.cj.jdbc.Driver");
             Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/A1_professor_schema","root","Hello@123");
             Statement stmt = con.createStatement();
             stmt.executeUpdate(stmnt);
+            ResultSet r = stmt.executeQuery("show index from professor");
+            while(r.next()){
+                System.out.println(r.getString(3) + "\t" + r.getString(5));
+            }
             con.close();
         }catch(Exception e){System.out.println(e);}
     }
@@ -46,13 +60,60 @@ public class exe {
         }catch(Exception e){System.out.println(e);}
     }
 
+    // write in writeups[ only avaliable in oracle not mysql ]
     void create_synonym(){
         try{
             String stmnt = "CREATE TABLE employees (emp_no INT AUTO_INCREMENT PRIMARY KEY,f_name VARCHAR(50),l_name VARCHAR(50));";
             Class.forName("com.mysql.cj.jdbc.Driver");
             Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/A1_professor_schema","root","Hello@123");
             Statement stmt = con.createStatement();
+            stmt.executeUpdate("drop table if exists employees");
             stmt.executeUpdate(stmnt);
+            con.close();
+        }catch(Exception e){System.out.println(e);}
+    }
+
+    void create_complex_view(){
+        try{
+            String stmnt = "create or replace view comp as select p.fname, p.lname from professor p, department d where p.dept_id=d.dept_id";
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/A1_professor_schema","root","Hello@123");
+            Statement stmt = con.createStatement();
+            stmt.executeUpdate(stmnt);
+            ResultSet r = stmt.executeQuery("select * from comp");
+            while(r.next()) {
+                System.out.println(r.getString(1) + "\t\t\t" + r.getString(2));
+            }
+            con.close();
+        }catch(Exception e){System.out.println(e);}
+    }
+
+    void create_compound_index(){
+        try{
+            String stmnt = "CREATE INDEX ind_2 ON professor(fname, lname)";
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/A1_professor_schema","root","Hello@123");
+            Statement stmt = con.createStatement();
+            stmt.executeUpdate(stmnt);
+            ResultSet r = stmt.executeQuery("show index from professor");
+            while(r.next()){
+                System.out.println(r.getString(3) + "\t" + r.getString(5));
+            }
+            con.close();
+        }catch(Exception e){System.out.println(e);}
+    }
+
+    void create_unique_index(){
+        try{
+            String stmnt = "CREATE unique INDEX ind_3 ON department(dept_id)";
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/A1_professor_schema","root","Hello@123");
+            Statement stmt = con.createStatement();
+            stmt.executeUpdate(stmnt);
+            ResultSet r = stmt.executeQuery("show index from department");
+            while(r.next()){
+                System.out.println(r.getString(3) + "\t" + r.getString(5));
+            }
             con.close();
         }catch(Exception e){System.out.println(e);}
     }
@@ -63,10 +124,12 @@ public class exe {
             Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/A1_professor_schema","root","Hello@123");
             Statement stmt = con.createStatement();
 
-            stmt.executeUpdate("drop table if exists employees;");
+            stmt.executeUpdate("drop table if exists employees");
             stmt.executeUpdate("drop index ind_1 on professor");
-            stmt.executeUpdate("DROP VIEW IF EXISTS comp;");
-            stmt.executeUpdate("drop table if exists sample;");
+            stmt.executeUpdate("drop index ind_2 on professor");
+            stmt.executeUpdate("drop index ind_3 on department");
+            stmt.executeUpdate("DROP VIEW IF EXISTS comp");
+            stmt.executeUpdate("drop table if exists sample");
 
             con.close();
         }catch(Exception e){System.out.println(e);}
