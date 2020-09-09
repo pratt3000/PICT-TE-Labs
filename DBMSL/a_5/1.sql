3ec2302a7c2c6765644467eecbd64f5632ec045c
pratt3000 <pratt3000@gmail.com>
2020-09-09 13:17:16 +0530

    A5 almost done

--- DBMSL/a_5/1.sql
+++ DBMSL/a_5/1.sql
@@ -5,7 +5,7 @@ use A5;
 create table Customer(
                     Cust_id int not null,
                     Name varchar(30),
-                    DateOfPayment varchar(10),
+                    DateOfPayment date,
                     NameOfScheme varchar(20),
                     Status varchar(10),
                     primary key(Cust_id)
@@ -13,24 +13,56 @@ create table Customer(
 
 create table Fine(
                     Cust_id int not null,
-                    Date varchar(10),
+                    Date date,
                     Amt int,
                     foreign key(Cust_id) references Customer(Cust_id) on delete cascade
                     );
 
-insert into Customer VALUES(1, "Prathamesh", "2000-04-8", "High-return", "Positive" );
-insert into Customer VALUES(2, "Aditya", "2000-03-15", "Low-return", "Positive" );
+insert into Customer VALUES(1, "Prathamesh", "2000-04-8", "High-return", "Negative" );
+insert into Customer VALUES(2, "Aditya", "2000-03-15", "Low-return", "Negative" );
 insert into Customer VALUES(3, "Sourav", "2000-03-12", "High-return", "Negative" );
 insert into Customer VALUES(4, "Rajesh", "2000-03-1", "Low-return", "Negative" );
 insert into Customer VALUES(5, "Suman", "2000-03-27", "Low-return", "Negative" );
 
 select * from Customer;
--- set date to 2000-03-10
-create PROCEDURE setFine(in DateF varchar(10))
-                    BEGIN
-                    declare myDate int;
-                    declare myCost int;
-                    set myDate=DateF;
-                    if myDate>"2000-03-25" AND myDate<"2000-04-9" THEN
-                    myCost=(5*(myDate-"2000-03-25"))
-                    -- incomplete, how to compare date??
+
+delimiter @@
+
+create PROCEDURE setFine(IN id int, IN NameOfScheme varchar(20))
+    BEGIN
+    declare myFine INT;
+    declare myDate date;
+    declare myStatus VARCHAR(10);
+    declare days int;
+    declare diff int;
+    select DateOfPayment into myDate FROM Customer where Cust_id = id;
+    SELECT Status into myStatus FROM Customer where Cust_id = id;
+    select DATEDIFF(CURDATE() , myDate) into diff;
+
+    declare exit handler for 1062
+    select 'Error : Duplicate' as message;
+
+    declare exit handler for not found
+    select 'Error :  Record not found' as message;
+
+    IF myStatus="Negative" THEN
+        IF diff>15 AND diff<=30 THEN
+            set myFine = 5*diff;
+        END IF
+        IF diff>30 THEN
+            set myFine = 50*(diff-30) + 150;
+        END IF
+
+        INSERT INTO Fine VALUES(id, myDate, myFine);
+        UPDATE Customer set Status="Positive" where Cust_id = id;
+
+    END IF;
+END;
+@@
+
+
+
+
+
+
+
