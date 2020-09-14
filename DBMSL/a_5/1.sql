88443ccce414d0c0dfa02a7a1f33b8a0eebea5f6
pratt3000 <pratt3000@gmail.com>
2020-09-14 11:16:00 +0530

    A5 done fully

--- DBMSL/a_5/1.sql
+++ DBMSL/a_5/1.sql
@@ -1,6 +1,7 @@
 drop database if exists A5;
 create database A5;
 use A5;
+DROP PROCEDURE IF EXISTS setFine;
 
 create table Customer(
                     Cust_id int not null,
@@ -18,47 +19,48 @@ create table Fine(
                     foreign key(Cust_id) references Customer(Cust_id) on delete cascade
                     );
 
-insert into Customer VALUES(1, "Prathamesh", "2000-04-8", "High-return", "Negative" );
-insert into Customer VALUES(2, "Aditya", "2000-03-15", "Low-return", "Negative" );
-insert into Customer VALUES(3, "Sourav", "2000-03-12", "High-return", "Negative" );
-insert into Customer VALUES(4, "Rajesh", "2000-03-1", "Low-return", "Negative" );
-insert into Customer VALUES(5, "Suman", "2000-03-27", "Low-return", "Negative" );
+insert into Customer VALUES(1, "Prathamesh", "2020-04-8", "High-return", "N" );
+insert into Customer VALUES(2, "Aditya", "2020-03-15", "Low-return", "N" );
+insert into Customer VALUES(3, "Sourav", "2020-03-12", "High-return", "N" );
+insert into Customer VALUES(4, "Rajesh", "2020-03-1", "Low-return", "N" );
+insert into Customer VALUES(5, "Suman", "2020-03-27", "Low-return", "N" );
 
-select * from Customer;
 
 delimiter @@
-
+select * from Customer@@
 create PROCEDURE setFine(IN id int, IN NameOfScheme varchar(20))
-    BEGIN
+BEGIN
     declare myFine INT;
     declare myDate date;
     declare myStatus VARCHAR(10);
     declare days int;
     declare diff int;
+
+    declare exit handler for 1062
+    select 'Error : Duplicate' as message;
+    declare exit handler for not found
+    select 'Error :  Record not found' as message;
+    
     select DateOfPayment into myDate FROM Customer where Cust_id = id;
     SELECT Status into myStatus FROM Customer where Cust_id = id;
     select DATEDIFF(CURDATE() , myDate) into diff;
 
-    declare exit handler for 1062
-    select 'Error : Duplicate' as message;
-
-    declare exit handler for not found
-    select 'Error :  Record not found' as message;
-
-    IF myStatus="Negative" THEN
+    IF myStatus="N" THEN
         IF diff>15 AND diff<=30 THEN
             set myFine = 5*diff;
-        END IF
+        END IF;
         IF diff>30 THEN
-            set myFine = 50*(diff-30) + 150;
-        END IF
+            set myFine = 50*(diff-30) + 75;
+        END IF;
 
         INSERT INTO Fine VALUES(id, myDate, myFine);
-        UPDATE Customer set Status="Positive" where Cust_id = id;
+        UPDATE Customer set Status="P" where Cust_id = id;
 
     END IF;
-END;
-@@
+END @@
+delimiter ;
+
+
 
 
 
